537c0f49a3d307d523f1a38178ce69d715655469
Yusuf <66310246+yusufshappy@users.noreply.github.com>
2021-04-12 13:34:12 +0300

    Update sex.js

--- pages/sex.js
+++ pages/sex.js
@@ -9,23 +9,23 @@ class Sex extends Component {
         <h1>31'in faydaları</h1>
 <ul>
 <li>Stresi Azaltır</li>
-<br>
+<br/>
 <li>Mutluluk Artar</li>
-<br>
+<br/>
 <li>Uyku Artar</li>
-<br>
+<br/>
   <li>Haftada ortalama 5-7 defa 31 çeken insanlarda kalp krizi, enfarktüs, miyokardiyal iskemi riskleri azalır.</li>
-  <br>  
+  <br/>  
 <li>Düzenli 31 çeken insanların prostat kanserine yakalanma ihtimalleri azalır. Özellikle 20'li yaşlarda haftada 5 ya da daha fazla defa 31 çeken (veya genel olarak boşalan) kişilerde bu risk minimuma iner. Ancak bu sayı abartılırsa kansere yakalanma oranı artar.</li>
-  <br>    
+  <br/>    
 <li>Aynı zamanda 31 çeken kişilerin, sırf bu hissi yaşamak için tanımadıkları insanlarla seks yaparak cinsel yolla bulaşan hastalıklara yakalanma riskleri azalır.</li> 
-   <br>  
+   <br/>  
   <li>Bunların yanı sıra, 31 sayesinde sikiş miktarı azaltılarak, istenmedik doğumların da önüne geçilir.</li>
-   <br>  
+   <br/>  
   <li>31 veya sikiş yoluyla düzenli boşalmak, sperm kalitesi ve hareketliliğini arttırır. Bu da cinsel gücün artması demektir.</li>
-   <br>  
+   <br/>  
   <li>Düzenli çekilen 31, daralmış damarların ve solunum yollarının açılmasını ve açık tutulmasını sağlar.</li>
-<br>
+<br/>
   <li>Çok yapıldığında sağır ettiği söyleniyor.(diyenlerin yalancısıyım hiç denemedim.)</li>
 </ul>
 
