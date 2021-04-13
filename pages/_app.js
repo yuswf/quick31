587571edb06568d7663115f234d26fe907993aa7
Yusuf <66310246+yusufshappy@users.noreply.github.com>
2021-04-13 20:11:10 +0300

    Delete pages directory

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import '../styles/globals.css';
-import '../styles/styles.css';
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
-
-export default MyApp;
